6bbb9812ba733249126e677b273e66eb81ea264f
Neha <neha309shar@gmail.com>
2025-03-31 20:14:48 +0530

    new

---
+++ pract7.dart
@@ -0,0 +1,22 @@
+//loops
+
+//1-for loops
+void main() {
+  for (int i = 10; i >= 0; i--) {
+    print(i);
+  }
+
+  //2-while loop
+  int j = 10;
+  while (j <= 200) {
+    print(j);
+    j++;
+  }
+
+  //3-do while
+  int s = 0;
+  do {
+    print(s);
+    s++;
+  } while (s <= 16);
+}
